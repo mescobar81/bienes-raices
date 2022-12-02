ef9e972424424709a63a9f0c15a5b4ed0ee97112
angel.me81@gmail.com <angel.me81@gmail.com>
2022-12-02 17:01:45 -0300

    Se agrega proteccion de rutas

--- middlewares/protegerRuta.js
+++ middlewares/protegerRuta.js
@@ -1,6 +1,24 @@
+import jwt from 'jsonwebtoken';
+import { Usuario } from '../models/index.js';
 
-const portegerRuta = (req, res, next) =>{
-    console.log('Desde Middleware');
+
+const portegerRuta = async (req, res, next) =>{
+    
+    //verificar si hay un token
+    const {_token} = req.cookies;
+    if(!_token){
+        return res.redirect('/bienes-raices/auth/login');
+    }
+
+    //comprobar el token
+    try {
+        const decode = jwt.verify(_token, process.env.TOKEN_SECRET);
+
+        const usuario = await Usuario.findByPK(decode.id);
+        console.log(usuario);
+    } catch (error) {
+        return res.clearCookie('_token').redirect('/bienes-raices/auth/login');
+    }
     next();
 }
 
